26e81ef8f92eaf9b945cacec3d67d89d8c47e0fd
Gary <gary.leeming@gmail.com>
2012-10-19 21:08:53 +0100

    Modified import.py

--- Test/import.py
+++ Test/import.py
@@ -1,5 +1,9 @@
 from sys import argv
 script, first, second, third = argv
+print "What is your name?",
+name = raw_input()
+
 print "The script is called: ", script
 print "The first argument is: ", first
 print "The last argument is: ", third
+print "Well done, ", name
