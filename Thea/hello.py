07249c943331fa6c9734e7a53e0e885a8bd7ef00
Gary Leeming <gary.leeming@gmail.com>
2012-10-25 20:18:17 +0100

    Update

--- Thea/hello.py
+++ Thea/hello.py
@@ -7,26 +7,29 @@ espeak.synth("Starting")
 time.sleep(0.2)
 os.system('clear') 
 
+
 t = datetime.now().strftime("%k %M")
+prefix = "%s " %t
 
 message1 = ["What is your name?", "Hello "]
 message2 = ["How old are you?", " you are "]
 
-espeak.synth(message1[0])
+espeak.synth(prefix + message1[0])
 print message1[0],
 reply1 = raw_input()
 time.sleep(2)
 
-espeak.synth(message2[0])
+espeak.synth(prefix + message2[0])
 print message2[0],
 reply2 = raw_input()
 time.sleep(2)
 
 print "%s %s, %s %s " %(message1[1], reply1, message2[1], reply2)
 
-prefix = "Hello world      "
 checkThea = reply1.replace("thea", "[[THEIuh]]")
 checkTHea = checkThea.replace("Thea", "[[THEIuh]]")
 
 espeak.synth("%s %s %s, %s %s " %(prefix, message1[1], checkThea, message2[1], reply2))
+
+raw_input(">")
 
