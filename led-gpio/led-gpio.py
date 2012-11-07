472112b6c168800ff0d2e2bf2ac7892aeb058a06
Gary Leeming <gary.leeming@gmail.com>
2012-11-07 19:09:34 +0000

    Added fix for GPIO

--- led-gpio/led-gpio.py
+++ led-gpio/led-gpio.py
@@ -1,14 +1,27 @@
+from time import sleep
 import RPi.GPIO as GPIO
+GPIO.setmode(GPIO.BOARD)
 GPIO.setup(13, GPIO.OUT)
 GPIO.setup(11, GPIO.IN)
 GPIO.setup(15, GPIO.OUT)
 GPIO.setup(12, GPIO.IN)
+GPIO.setup(16, GPIO.OUT)
 while 1:
 	if GPIO.input(11):
 		GPIO.output(13, False)
+		sleep(0.2)
+		GPIO.output(13, True)
+		GPIO.output(15, False)
+		sleep(0.2)
+		GPIO.output(15, True)
+		GPIO.output(16, False)
+		sleep(0.2)
+		GPIO.output(16, True)
 	else:
 		GPIO.output(13, True)
-		
+		GPIO.output(15, True)
+		GPIO.output(16, True)
+
 	if GPIO.input(12):
 		GPIO.output(15, False)
 	else:
