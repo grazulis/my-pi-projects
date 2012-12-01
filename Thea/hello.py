d004b70138e2d22446dcb6c2ca635d3734ce8d23
Gary Leeming <gary.leeming@gmail.com>
2012-12-01 13:28:09 +0000

    Small edits

--- Thea/hello.py
+++ Thea/hello.py
@@ -4,11 +4,11 @@ import time
 import os
 
 espeak.synth("Starting")
-time.sleep(0.2)
+time.sleep(0.5)
 os.system('clear') 
 
 
-t = datetime.now().strftime("%k %M")
+t = ""
 prefix = "%s " %t
 
 message1 = ["What is your name?", "Hello "]
@@ -27,9 +27,9 @@ time.sleep(2)
 print "%s %s, %s %s " %(message1[1], reply1, message2[1], reply2)
 
 checkThea = reply1.replace("thea", "[[THEIuh]]")
-checkTHea = checkThea.replace("Thea", "[[THEIuh]]")
+checkThea = checkThea.replace("Thea", "[[THEIuh]]")
 
-espeak.synth("%s %s %s, %s %s " %(prefix, message1[1], checkThea, message2[1], reply2))
+espeak.synth("%s %s %s, %s %s Do you need a wee? " %(prefix, message1[1], checkThea, message2[1], reply2))
 
 raw_input(">")
 
