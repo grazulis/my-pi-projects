5a905e3ff40dc56dcadba61d9c3fe252bd276882
grazulis <gary.leeming@gmail.com>
2012-12-14 18:43:52 +0000

    OK, so need to pass variable into function

--- led-gpio/led-speed.py
+++ led-gpio/led-speed.py
@@ -8,7 +8,7 @@ GPIO.setup(12, GPIO.IN)
 GPIO.setup(16, GPIO.OUT)
 speed = 5
 
-def checkbutton():                                
+def checkbutton(speed):                                
 	if GPIO.input(11) and speed > 1:
 		speed = speed - 1	
 	if GPIO.input(12) and speed < 10:
@@ -18,7 +18,7 @@ while 1:
         pins = [13, 16, 15]
         
         for pin in pins:
-                checkbutton()
+                checkbutton(speed)
                 GPIO.output(pin, False)
                 sleep(speed * 0.1)
                 GPIO.output(pin, True)
