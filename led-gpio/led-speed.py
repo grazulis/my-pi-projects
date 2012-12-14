00ff01da54ead28e726bc323edc26f3e008fcf80
Gary Leeming <gary.leeming@gmail.com>
2012-12-14 18:33:25 +0000

    Some fixes

--- led-gpio/led-speed.py
+++ led-gpio/led-speed.py
@@ -6,21 +6,24 @@ GPIO.setup(11, GPIO.IN)
 GPIO.setup(15, GPIO.OUT)
 GPIO.setup(12, GPIO.IN)
 GPIO.setup(16, GPIO.OUT)
-var speed = 5
+speed = 5
 while 1:
-
+	checkbutton()
         sleep(speed * 0.1)
         GPIO.output(13, True)
-        GPIO.output(15, False)
-        sleep(speed * 0.1)
-        GPIO.output(15, True)
         GPIO.output(16, False)
-        sleep(speed * 0.1)
+        checkbutton()
+	sleep(speed * 0.1)
         GPIO.output(16, True)
-                                
-	if GPIO.input(11) && speed > 1:
-		speed = speed - 1
-	
-	if GPIO.input(12) && speed < 10:
+        GPIO.output(15, False)
+        checkbutton()
+	sleep(speed * 0.1)
+        GPIO.output(15, True)
+	GPIO.output(13, False)
+
+def checkbutton():                                
+	if GPIO.input(11) and speed > 1:
+		speed = speed - 1	
+	if GPIO.input(12) and speed < 10:
 		speed = speed + 1
 	
