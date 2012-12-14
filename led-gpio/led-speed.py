b821ebbb2ed49e1f3a00423540a331a66796245e
grazulis <gary.leeming@gmail.com>
2012-12-14 18:46:11 +0000

    But don't know how to get it out again... So removed it

--- led-gpio/led-speed.py
+++ led-gpio/led-speed.py
@@ -7,18 +7,15 @@ GPIO.setup(15, GPIO.OUT)
 GPIO.setup(12, GPIO.IN)
 GPIO.setup(16, GPIO.OUT)
 speed = 5
-
-def checkbutton(speed):                                
-	if GPIO.input(11) and speed > 1:
-		speed = speed - 1	
-	if GPIO.input(12) and speed < 10:
-		speed = speed + 1
-		
+			
 while 1:
         pins = [13, 16, 15]
         
         for pin in pins:
-                checkbutton(speed)
+                if GPIO.input(11) and speed > 1:
+                        speed = speed - 1	
+                if GPIO.input(12) and speed < 10:
+                        speed = speed + 1
                 GPIO.output(pin, False)
                 sleep(speed * 0.1)
                 GPIO.output(pin, True)
