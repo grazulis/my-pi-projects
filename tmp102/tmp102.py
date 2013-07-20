1efe8251bf193badd25de727e9662e4ffdacf43e
Gary Leeming <gary.leeming@gmail.com>
2013-07-20 10:31:50 +0100

    Removed loop so can set up cron job

--- tmp102/tmp102.py
+++ tmp102/tmp102.py
@@ -3,11 +3,10 @@ import time
 import datetime
 
 bus = smbus.SMBus(1)
-while 1:
-	data = bus.read_i2c_block_data(0x48, 0)
-	msb = data[0]
-	lsb = data[1]
-	# Print degrees Celsius
-	print (((msb << 8) | lsb) >> 4) * 0.0625
-	print datetime.datetime.now()
-	time.sleep(60)
+
+data = bus.read_i2c_block_data(0x48, 0)
+msb = data[0]
+lsb = data[1]
+# Print degrees Celsius
+out = (((msb << 8) | lsb) >> 4) * 0.0625
+print "%f|%s"%( out, datetime.datetime.now()) 
